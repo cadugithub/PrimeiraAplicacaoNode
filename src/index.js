4cc50eb8d5e7ef5fd63cb890490f6207c9c5c013
cadugithub <carlosk2k2@gmail.com>
2022-06-30 10:05:27 -0300

    implementação de método para excluir a conta de um cliente

--- src/index.js
+++ src/index.js
@@ -119,4 +119,16 @@ app.put("/account", verifyIfExistsAccountCPF ,(request, response)=>{
  return response.status(201).send()
 })
 
+app.get("/account", verifyIfExistsAccountCPF,(request, response)=>{
+    const { customer } = request
+    return response.json(customer)
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response)=>{
+    const {customer} = request
+
+    customers.splice(customer, 1)
+
+    return response.status(200).json(customers)
+})
 app.listen(3333) // localhost:3333
